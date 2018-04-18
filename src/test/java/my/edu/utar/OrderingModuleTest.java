04101fe389650ba40e6879beda3178e2e065de68
DheenodaraRao <dheenodararao@gmail.com>
2018-04-18 22:47:21 +0800

    Completed the test methods with test data from txt file

--- src/test/java/my/edu/utar/OrderingModuleTest.java
+++ src/test/java/my/edu/utar/OrderingModuleTest.java
@@ -4,6 +4,13 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
@@ -16,53 +23,254 @@ import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class OrderingModuleTest {
-	
-	OrderingModule io = mock(OrderingModule.class);
+/*	
+	public static void main(String[] args){
+		String fileName = "orderTestIllegal1.txt";
 
+        // This will reference one line at a time
+        String line = null;
+        ArrayList<Object> list = new ArrayList<Object>();
+        
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            int lines = 0;
+            while((line = bufferedReader.readLine()) != null) {
+            	boolean op1 = Boolean.parseBoolean(line);
+        		boolean op2 = Boolean.parseBoolean(bufferedReader.readLine());	
+        		int quantity = Integer.parseInt(bufferedReader.readLine());
+        		
+        		list.add(new Object[]{op1,op2,quantity});
+            }  
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+        
+        for(int i = 0; i < list.size(); i++){
+        	Object[] data = (Object[]) list.get(i);
+        	System.out.println(data[0] + " " + data[1] + " " + data[2]);
+        }
+        
+        Object[] data = new Object[list.size()];
+        for(int i = 0; i < list.size(); i++){
+        	data[i] = (Object[]) list.get(i);
+        }
+    }
+*/
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void IllegalArgumentMockito1(){
-		OrderingModule mod = new OrderingModule();
-		OrderingModule spymod = spy(mod);
-		
-		doReturn(new boolean[] {true, false}).when(spymod).getOrderOptions();
-		doReturn(0).when(spymod).getOrderQuantity();
-		
-		boolean[] actual = spymod.getOrderOptions();
-		int quantity = spymod.getOrderQuantity();
-		
-		spymod.createOrders(actual[0], actual[1], quantity);
-	}	
+	public Object[] getParamIllegal1(){
+		String fileName = "orderTestIllegal1.txt";
+
+        // This will reference one line at a time
+        String line = null;
+        ArrayList<Object> list = new ArrayList<Object>();
+        
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            int lines = 0;
+            while((line = bufferedReader.readLine()) != null) {
+            	boolean op1 = Boolean.parseBoolean(line);
+        		boolean op2 = Boolean.parseBoolean(bufferedReader.readLine());	
+        		int quantity = Integer.parseInt(bufferedReader.readLine());
+        		
+        		list.add(new Object[]{op1,op2,quantity});
+            }  
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+        
+        for(int i = 0; i < list.size(); i++){
+        	Object[] data = (Object[]) list.get(i);
+        	System.out.println(data[0] + " " + data[1] + " " + data[2]);
+        }
+        
+        Object[] data = new Object[list.size()];
+        for(int i = 0; i < list.size(); i++){
+        	data[i] = (Object[]) list.get(i);
+        }
+        
+        return data;
+    }
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void IllegalArgumentMockito2(){
-		OrderingModule mod = new OrderingModule();
-		OrderingModule spymod = spy(mod);
-		
-		doReturn(new boolean[] {true, false}).when(spymod).getOrderOptions();
-		doReturn(100).when(spymod).getOrderQuantity();
-		
-		boolean[] actual = spymod.getOrderOptions();
-		int quantity = spymod.getOrderQuantity();
-		
-		spymod.createOrders(actual[0], actual[1], quantity);
-	}	
+	public Object[] getParamIllegal2(){
+		String fileName = "orderTestIllegal2.txt";
+
+        // This will reference one line at a time
+        String line = null;
+        ArrayList<Object> list = new ArrayList<Object>();
+        
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+            	boolean op1 = Boolean.parseBoolean(line);
+        		boolean op2 = Boolean.parseBoolean(bufferedReader.readLine());	
+        		int quantity = Integer.parseInt(bufferedReader.readLine());
+        		
+        		list.add(new Object[]{op1,op2,quantity});
+            }  
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+        
+        for(int i = 0; i < list.size(); i++){
+        	Object[] data = (Object[]) list.get(i);
+        	System.out.println(data[0] + " " + data[1] + " " + data[2]);
+        }
+        
+        Object[] data = new Object[list.size()];
+        for(int i = 0; i < list.size(); i++){
+        	data[i] = (Object[]) list.get(i);
+        }
+        
+        return data;
+    }
 	
 	public Object[] getParamLegal(){
-		return new Object[]{
-			new Object[]{
-					true,false,1, new OrderItem(true, false, 1)
-			},
-			new Object[]{
-					true,false,99, new OrderItem(true, false, 99)
-			}
-		};
+		String fileName = "orderTestLegal.txt";
+
+        // This will reference one line at a time
+        String line = null;
+        ArrayList<Object> list = new ArrayList<Object>();
+        
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            int lines = 0;
+            while((line = bufferedReader.readLine()) != null) {
+            	boolean op1 = Boolean.parseBoolean(line);
+        		boolean op2 = Boolean.parseBoolean(bufferedReader.readLine());	
+        		int quantity = Integer.parseInt(bufferedReader.readLine());
+        		
+        		list.add(new Object[]{op1,op2,quantity, new OrderItem(op1, op2, quantity)});
+            }  
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+        
+        for(int i = 0; i < list.size(); i++){
+        	Object[] data = (Object[]) list.get(i);
+        	System.out.println(data[0] + " " + data[1] + " " + data[2]);
+        }
+        
+        Object[] data = new Object[list.size()];
+        for(int i = 0; i < list.size(); i++){
+        	data[i] = (Object[]) list.get(i);
+        }
+        
+        return data;
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method ="getParamIllegal1")
+	public void TestIllegalArgumentMockito1(boolean op1, boolean op2, int pQuantity){
+		OrderingModule mod = new OrderingModule();
+		OrderingModule spymod = spy(mod);
+		
+		doReturn(new boolean[] {op1, op2}).when(spymod).getOrderOptions();
+		doReturn(pQuantity).when(spymod).getOrderQuantity();
+		
+		boolean[] actual = spymod.getOrderOptions();
+		int quantity = spymod.getOrderQuantity();
+		
+		spymod.createOrders(actual[0], actual[1], quantity);
+	}	
 	
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method ="getParamIllegal2")
+	public void TestIllegalArgumentMockito2(boolean op1, boolean op2, int pQuantity){
+		OrderingModule mod = new OrderingModule();
+		OrderingModule spymod = spy(mod);
+		
+		doReturn(new boolean[] {op1, op2}).when(spymod).getOrderOptions();
+		doReturn(pQuantity).when(spymod).getOrderQuantity();
+		
+		boolean[] actual = spymod.getOrderOptions();
+		int quantity = spymod.getOrderQuantity();
+		
+		spymod.createOrders(actual[0], actual[1], quantity);
+	}	
+		
 	@Test
 	@Parameters(method="getParamLegal")
-	public void testSpy(boolean op1, boolean op2, int pQuantity, OrderItem expectedItem){
+	public void testLegalArguements(boolean op1, boolean op2, int pQuantity, OrderItem expectedItem){
 		
 		OrderingModule mod = new OrderingModule();
 		OrderingModule spymod = spy(mod);
